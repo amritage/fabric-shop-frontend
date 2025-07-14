041087f0db98c26ff1075580b30ad334fe78b234
Madhu Pandey <madhupandey29052004@gmail.com>
2025-07-14 14:30:34 +0530

    fix chaining error

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -17,6 +17,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={jost.variable}>
       <head>
+        {/* Preload critical CSS */}
+        <link rel="preload" href="/assets/css/font-awesome-pro.css" as="style" onLoad="this.rel='stylesheet'" />
+        {/* Preload critical font files */}
+        <link rel="preload" href="/assets/fonts/fa-regular-400.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+        <link rel="preload" href="/assets/fonts/fa-brands-400.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+        <link rel="preload" href="/assets/fonts/fa-light-300.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+        <link rel="preload" href="/assets/fonts/fa-solid-900.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
         <GoogleAnalytics />
         <MicrosoftClarity />
         <meta
