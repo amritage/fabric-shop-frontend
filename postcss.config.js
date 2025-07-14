c309d7cb62522408578e445ce34b11ea013ab9cf
Madhu Pandey <madhupandey29052004@gmail.com>
2025-07-14 17:49:05 +0530

    fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const purgecss = require('@fullhuman/postcss-purgecss');
 
-const purge = purgecss({
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-    './public/**/*.html',
-  ],
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-});
-
 module.exports = {
   plugins: {
     'postcss-preset-env': {
@@ -17,6 +9,16 @@ module.exports = {
       },
       stage: 3,
     },
-    ...(process.env.NODE_ENV === 'production' ? { purge } : {}),
+    ...(process.env.NODE_ENV === 'production'
+      ? {
+          purgecss: purgecss({
+            content: [
+              './src/**/*.{js,jsx,ts,tsx}',
+              './public/**/*.html',
+            ],
+            defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+          }),
+        }
+      : {}),
   },
 };
