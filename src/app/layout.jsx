c021f833d7dba81bfc37e9c3fe08be3d685fce91
Madhu Pandey <madhupandey29052004@gmail.com>
2025-07-15 11:50:16 +0530

    fix

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -27,6 +27,26 @@ export default function RootLayout({ children }) {
         <link rel="preload" href="/assets/css/font-awesome-pro.css" as="style" onLoad="this.rel='stylesheet'" />
         {/* Preload only the most critical font */}
         <link rel="preload" href="/assets/fonts/fa-regular-400.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+        {/* Preload Jost variable fonts for faster text rendering */}
+        <link
+          rel="preload"
+          href="/assets/fonts/Jost/Jost-VariableFont_wght.ttf"
+          as="font"
+          type="font/ttf"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/assets/fonts/Jost/Jost-Italic-VariableFont_wght.ttf"
+          as="font"
+          type="font/ttf"
+          crossOrigin="anonymous"
+        />
+        {/* Preload all used Font Awesome weights */}
+        <link rel="preload" href="/assets/fonts/fa-brands-400.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+        <link rel="preload" href="/assets/fonts/fa-regular-400.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+        <link rel="preload" href="/assets/fonts/fa-solid-900.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+        <link rel="preload" href="/assets/fonts/fa-light-300.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
         <GoogleAnalytics />
         <MicrosoftClarity />
         <meta
