eb7d6670097449327ed6997abed66fdcd273c5b5
Madhu Pandey <madhupandey29052004@gmail.com>
2025-07-17 19:05:49 +0530

    fix

--- OneDrive/Desktop/Amrita global enterprise/admin-panel/src/app/components/tooltip/edit-tooltip.tsx
+++ OneDrive/Desktop/Amrita global enterprise/admin-panel/src/app/components/tooltip/edit-tooltip.tsx
@@ -10,7 +10,6 @@ const EditTooltip = ({ showEdit }: { showEdit: boolean }) => {
       <span className="relative z-10 p-2 text-tiny leading-none font-medium text-white whitespace-no-wrap w-max bg-slate-800 rounded py-1 px-2 inline-block">
         Edit
       </span>
-      <div className="w-3 h-3 -mt-2 rotate-45 bg-black"></div>
     </div>
   );
 };
